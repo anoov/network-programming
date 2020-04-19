ae93ccc6e8b1a29495d9b096e1f64b73f7472830
anoov <1374206329@qq.com>
2020-04-19 21:25:57 +0800

    解决客户端粘包问题

--- client.cpp
+++ client.cpp
@@ -33,18 +33,25 @@ public:
     void Close();
     //发送数据
     int SendData(DataHeader *header);
-    //接收数据
+    //接收数据 处理粘包 拆分包
     int RecvData();
-    //处理网络消息
-    void OnNetMsg(DataHeader *header, char *szRecv);
+    //响应网络消息
+    virtual void OnNetMsg(DataHeader *data);
     bool OnRun();
+    //是否工作
     bool isRun();
     SOCKET Get() {
         return _sock;
     }
 private:
     SOCKET _sock;
-
+    //以下解决粘包和拆分包需要的变量
+    //接收缓冲区
+    static const int RECV_BUFF_SIZE = 10240;
+    char _szRecv[RECV_BUFF_SIZE] = {};
+    //第二缓冲区  消息缓冲区
+    char _szMsgBuf[RECV_BUFF_SIZE*10] = {};
+    int _lastPos = 0;                        //指向缓冲区有数据的末尾位置
 };
 
 void EasyTCPClient::Close() {
@@ -81,7 +88,7 @@ int EasyTCPClient::Connect(char *ip, unsigned short port) {
     if (ret == SOCKET_ERROR) {
         printf("连接失败...\n");
     }else {
-        printf("连接成功...\n");
+        printf("<socket = %d>连接成功...\n", _sock);
     }
     return ret;
 }
@@ -95,12 +102,14 @@ bool EasyTCPClient::OnRun() {
         int selectRet = select(_sock + 1, &fdReader, nullptr, nullptr, &t);
         if (selectRet < 0) {
             printf("<Socket = %d>select发生错误, 任务结束1!\n", _sock);
+            Close();
             return false;
         }
         if (FD_ISSET(_sock, &fdReader)) {
             FD_CLR(_sock, &fdReader);
             if (-1 == RecvData()) {
                 printf("<Socket = %d>select发生错误, 任务结束2!\n", _sock);
+                Close();
                 return false;
             }
         }
@@ -114,15 +123,31 @@ int EasyTCPClient::RecvData() {
     //5 接收客户端数据
     //5.1 接受数据头
     //使用缓冲区来接受数据
-    char szRecv[4096] = {};
-    int nLen = recv(_sock, szRecv, sizeof(DataHeader), 0);
-    auto* header = (DataHeader *)szRecv;
+    int nLen = recv(_sock, _szRecv, RECV_BUFF_SIZE, 0);
     if (nLen <= 0) {
-        printf("与服务器断开连接, 任务结束\n");
+        printf("<socket = %d>与服务器断开连接, 任务结束\n", _sock);
         return -1;
     }
-    recv(_sock, szRecv+sizeof(DataHeader), header->dataLength - sizeof(DataHeader), 0);
-    OnNetMsg(header, szRecv);
+    //将收取到的数据拷贝到消息缓冲区
+    memcpy(_szMsgBuf + _lastPos, _szRecv, nLen);
+    _lastPos += nLen;
+    //判断收到消息的长度是否大于消息头的长度，若大于消息头的长度，就可以取出消息体的长度
+    while (_lastPos >= sizeof(DataHeader)) {
+        auto *header = (DataHeader*)_szMsgBuf;
+        //判断收到消息的长度是否大于消息体的长度，若大于消息体的长度，就可以取出消息体
+        if (_lastPos >= header->dataLength) {
+            //此时消息缓冲中有两部分数据，一部分是待处理的数据，第二部分是下一次处理的数据
+            //消息缓冲区中未处理消息的长度
+            int nSize = _lastPos - header->dataLength;  //消息缓冲中收到的数据总长度减去将要处理的数据的长度
+            OnNetMsg(header);
+            //通过掩盖的方法将处理过的数据清除
+            memcpy(_szMsgBuf, _szMsgBuf + header->dataLength, nSize);
+            _lastPos = nSize;
+        } else {
+            //剩余的消息虽然大于消息头但是小于消息体
+            break;
+        }
+    }
     return 0;
 }
 
@@ -130,34 +155,33 @@ bool EasyTCPClient::isRun() {
     return _sock != INVALID_SOCKET;
 }
 
-void EasyTCPClient::OnNetMsg(DataHeader *header, char *szRecv) {
-    switch (header->cmd) {
+void EasyTCPClient::OnNetMsg(DataHeader *data) {
+    switch (data->cmd) {
         case CMD_LOGIN_RESULT:
         {
-            LoginResult* login = (LoginResult *)szRecv;
-            printf("收到服务器<Socket = %d>消息：CMD_LOGIN_RESULT, 数据长度: %d，用户名称: %d\n",
+            LoginResult* login = (LoginResult *)data;
+            printf("<Socket = %d>收到服务器消息：CMD_LOGIN_RESULT, 数据长度: %d，结果: %d\n",
                    _sock, login->dataLength, login->result);
         }
             break;
         case CMD_LOGOUT_RESULT:
         {
-            LoginOutResult* loginOut = (LoginOutResult *)szRecv;
-            printf("收到服务器<Socket = %d>消息：CMD_LOGOUT_RESULT, 数据长度: %d\n",
+            LoginOutResult* loginOut = (LoginOutResult *)data;
+            printf("<Socket = %d>收到服务器消息：CMD_LOGOUT_RESULT, 数据长度: %d\n",
                    _sock ,loginOut->dataLength);
         }
             break;
         case CMD_NEW_USER_JOIN:
         {
-            NewUserJoin* newJoin = (NewUserJoin *)szRecv;
-            printf("收到服务器<Socket = %d>消息：NEW_USER_JOIN, 数据长度: %d\n",
+            NewUserJoin* newJoin = (NewUserJoin *)data;
+            printf("<Socket = %d>收到服务器消息：NEW_USER_JOIN, 数据长度: %d\n",
                    _sock ,newJoin->dataLength);
         }
             break;
         default:
         {
-            header->cmd = CMD_ERROR;
-            header->dataLength = 0;
-            send(_sock, &header, sizeof(DataHeader), 0);
+            printf("<Socket = %d>收到未定义消息：NEW_ERROR, 数据长度: %d\n",
+                   _sock ,data->dataLength);
         }
             break;
     }
