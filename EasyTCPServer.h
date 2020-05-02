1ac88d0fdd873181c8830d7e04cf9824b33e4e06
anoov <1374206329@qq.com>
2020-05-02 19:56:07 +0800

    为发送添加发送缓冲区，定量发送

--- EasyTCPServer.h
+++ EasyTCPServer.h
@@ -36,17 +36,48 @@ public:
     explicit ClientSocket(SOCKET s = INVALID_SOCKET){
         _sockFd = s;
         memset(_szMsgBuf, 0, sizeof(_szMsgBuf));
+        memset(_szSendBuf, 0, sizeof(_szSendBuf));
         _lastPos = 0;
+        _lastSendPos = 0;
     }
     SOCKET GetSock() {return _sockFd;}
     char *GetMsg() {return _szMsgBuf;}
     int GetPos()  {return _lastPos;}
     void SetPos(int pos) {_lastPos = pos;}
     int SendData(DataHeader *header) {
-        if (header) {
-            return send(_sockFd, header, header->dataLength, 0);
+        int ret = SOCKET_ERROR;
+        //要发送数据的长度
+        int nSendLen = header->dataLength;
+        //要发送的数据
+        const char* pSendData = (const char *)header;
+
+        while (true) {
+            //如何要发送的数据长度远大于缓冲区，需要循环发送
+            if (_lastSendPos + nSendLen >= SEND_BUFF_SIZE) {
+                //计算可拷贝的数据长度
+                int nCopyLen = SEND_BUFF_SIZE - _lastSendPos;
+                //拷贝数据
+                memcpy(_szSendBuf + _lastSendPos, pSendData, nCopyLen);
+                //计算剩余数据位置
+                pSendData += nCopyLen;
+                //计算剩余数据长度
+                nSendLen -= nCopyLen;
+                //发送数据
+                ret = send(_sockFd, _szSendBuf, SEND_BUFF_SIZE, 0);
+                //数据尾部指针清零
+                _lastSendPos = 0;
+                //如何发生错误
+                if (SOCKET_ERROR == ret) {
+                    return ret;
+                }
+            } else {
+                //将要发送的数据拷贝到发送缓冲区尾部
+                memcpy(_szSendBuf + _lastSendPos, pSendData, nSendLen);
+                _lastSendPos += nSendLen;
+                break;
+            }
         }
-        return SOCKET_ERROR;
+        return ret;
     }
 private:
     SOCKET _sockFd;
@@ -56,6 +87,13 @@ private:
     //第二缓冲区  消息缓冲区
     char _szMsgBuf[RECV_BUFF_SIZE*10] = {};
     int _lastPos;                        //指向缓冲区有数据的末尾位置
+
+    //发送缓冲区
+    static const int SEND_BUFF_SIZE = 10240 * 10;
+    char _szSendBuf[SEND_BUFF_SIZE] = {};
+    int _lastSendPos;                        //指向缓冲区有数据的末尾位置
+
+
 };
 
 //网络事件接口
@@ -226,7 +264,6 @@ void CellServer::Close() {
 int CellServer::RecvData(ClientSocket* clientSock) {
     int nLen = (int)recv(clientSock->GetSock(), _szRecv, RECV_BUFF_SIZE, 0);
     _pNetEvent->OnNetRecv(clientSock);
-    //auto* header = (DataHeader *)szRecv;
     if (nLen <= 0) {
         //printf("客户端<Socket = %d>退出, 任务结束\n", clientSock->GetSock());
         return -1;
@@ -317,11 +354,6 @@ private:
     SOCKET _sock;
     //消息处理对象，内部创建线程
     std::vector<CellServer*> _cellServer;
-    //第一缓冲区（第二缓冲区封装在ClientSocket里面）
-    static const int RECV_BUFF_SIZE = 10240;
-    char _szRecv[RECV_BUFF_SIZE] = {};
-
-    //static const int CELL_SERVER_THREAD_COUNT = 4;
 
     //每秒消息计时
     CELLTimeStamp _tTime;
